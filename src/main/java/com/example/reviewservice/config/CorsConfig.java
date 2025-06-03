1e18d16d0174db4ed69a839c91b4d1bfc183e724
Oladayo Richard Ayomide <Oladayorichard1@gmail.com>
2025-06-03 07:07:10 +0100

    added new controller

---
+++ src/main/java/com/example/reviewservice/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.reviewservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all routes
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://book-store-v2.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
